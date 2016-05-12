20e540cbb03c82ec52081e135e46c152fad345d0
pommi15 <pommi15@hotmail.com>
2016-05-12 19:31:56 +0200

    minor changes

--- import.h
+++ import.h
@@ -1,15 +1,15 @@
-#ifndef TXTIMPORT_H_
-#define TXTIMPORT_H_
+#ifndef IMPORT_H_
+#define IMPORT_H_
 #include <string>
 #include <iostream>
 #include <vector>
 #include <iostream>
 #include <fstream>
 
-class TxtImport {
+class Import {
  public:
-  TxtImport();  // constructor
-  std::vector<int> input_vector;  // vector for the read in numbers
+  Import();  // constructor
+  std::vector<std::string> input_vector;  // vector for the read in numbers
   void read(std::string filepath);  // function to read the numbers into the
 };
 #endif  // TXTIMPORT_H_
