7c059dd6b34a19f4b74fecf0827f0b40a7a16f86
Tobias Watzek <spitzfaust@gmail.com>
2016-05-14 11:45:22 +0200

    Added namegen and botorigin file

---
+++ namegen.cpp
@@ -0,0 +1,11 @@
+/*************************
+ *       d[ o_0 ]b       *
+ *  the lab - experiment *
+ *          by           *
+ *    Thomas RAUHOFER    *
+ *        if15b029       *
+ *          and          *
+ *     Tobias WATZEK     *
+ *        if15b038       *
+ *************************/
+#include "namegen.h"
---
+++ namegen.h
@@ -0,0 +1,27 @@
+/*************************
+ *       d[ o_0 ]b       *
+ *  the lab - experiment *
+ *          by           *
+ *    Thomas RAUHOFER    *
+ *        if15b029       *
+ *          and          *
+ *     Tobias WATZEK     *
+ *        if15b038       *
+ *************************/
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "import.h"
+
+class Namegen
+{
+public:
+  Namegen();
+  get_name();
+private:
+  std::vector<std::string> name_list;
+  std::vector<std::string> origin_list;
+
+};
