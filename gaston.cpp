7c32e69c378ca62dff730e08b5cf3794ecb0961d
pommi15 <pommi15@hotmail.com>
2016-05-17 20:39:36 +0200

    tried to fix gaston

--- gaston.cpp
+++ gaston.cpp
@@ -18,20 +18,23 @@ Gaston::Gaston(const std::shared_ptr<Labyrinth> maze)
 Gaston::Gaston(const std::shared_ptr<Labyrinth> maze, std::string name)
     : Bot(maze, name) {
   this->type = "Gaston";
-  this->facing = SOUTH;
 }
 
 void Gaston::make_step() {
   direction step = this->facing;
   position pos = this->current_pos;
+  int tmp;
   if (this->is_cul_de_sac(pos)) {
-    std::cout <<"FUCK cds" << this->step_counter << std::endl;
-    step = turn_180(step);
-  } else if (this->is_turn(pos)) {
-    std::cout <<"FUCK turn" << this->step_counter << std::endl;
-    step = this->turn_direction(pos);
-  } else if (this->is_straight(pos)) {
-    std::cout <<"FUCK straight" << this->step_counter << std::endl;
+    std::cout <<"cds" << this->step_counter << std::endl;
+
+    if(pos == this->maze->get_entry()){
+      this->place_markings[pos] = LAST;
+    }else{
+      step = turn_180(step);
+    }
+
+  }else if (this->is_straight(pos)) {
+    std::cout <<"straight" << this->step_counter << std::endl;
     /* if next ahead is place*/
     if(this->is_place(calc_coordinates())){
       /* tile is checked for markings */
@@ -39,15 +42,24 @@ void Gaston::make_step() {
         this->place_markings[pos] = LAST;
       }
     }
+  }else if (this->is_turn(pos)) {
+    std::cout <<"turn" << this->step_counter << std::endl;
+    step = this->turn_direction(pos);
+
   } else if (this->is_place(pos)) {
-    std::cout <<"FUCK place" << this->step_counter << std::endl;
+
+    std::cout <<"place" << this->step_counter << std::endl;
     /* place entrance is marked first if place has not been stepped into before*/
     step = place_choice(pos);
     this->place_markings[calc_coordinates()] = STOP;
   }
+
   this->current_pos = this->calc_coordinates(pos, step);
   this->history.push_back(this->current_pos);
   ++this->step_counter;
+  std::cout << "x : " << pos.x << " y : " << pos.y << std::endl;
+  std:: cout << "facing: " <<step << std::endl;
+  std::cin >> tmp;
 }
 
 direction Gaston::place_choice(position pos){
